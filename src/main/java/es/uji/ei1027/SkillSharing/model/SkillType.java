4002d87de7fec0b58b796398575d726fae643916
al394784 <al394784@uji.es>
2022-03-04 12:30:45 +0100

    Dao

---
+++ src/main/java/es/uji/ei1027/SkillSharing/model/SkillType.java
@@ -0,0 +1,55 @@
+package es.uji.ei1027.SkillSharing.model;
+
+
+
+
+public class SkillType {
+     private int skillType;
+     private  String name;
+     private String description;
+     private int level;
+
+     public SkillType(){}
+
+    public int getSkillType() {
+        return skillType;
+    }
+
+    public void setSkillType(int skillType) {
+        this.skillType = skillType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "SkillType{" +
+                "skillType=" + skillType +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", level=" + level +
+                '}';
+    }
+}
