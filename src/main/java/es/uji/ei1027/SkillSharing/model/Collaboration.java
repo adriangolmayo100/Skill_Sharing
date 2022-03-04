04dacf17744ce5cad9e7f60608282a68a889ce0e
imanolsm99 <al394825@uji.es>
2022-03-04 12:47:51 +0100

    Model collaboartion By Imanol Sosa

---
+++ src/main/java/es/uji/ei1027/SkillSharing/model/Collaboration.java
@@ -0,0 +1,72 @@
+package es.uji.ei1027.SkillSharing.model;
+
+import javax.xml.crypto.Data;
+
+public class Collaboration{
+    private int id_offer;
+    private Data start;
+    private Data finish;
+    private int rating;
+    private String comments;
+    private int idRequest;
+
+    public int getIdRequest() {
+        return idRequest;
+    }
+
+    public void setIdRequest(int idRequest) {
+        this.idRequest = idRequest;
+    }
+
+    public int getId_offer() {
+        return id_offer;
+    }
+
+    public void setId_offer(int id_offer) {
+        this.id_offer = id_offer;
+    }
+
+    public Data getStart() {
+        return start;
+    }
+
+    public void setStart(Data start) {
+        this.start = start;
+    }
+
+    public Data getFinish() {
+        return finish;
+    }
+
+    public void setFinish(Data finish) {
+        this.finish = finish;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return "Collaboration{" +
+                "id_offer=" + id_offer +
+                ", start=" + start +
+                ", finish=" + finish +
+                ", rating=" + rating +
+                ", comments='" + comments + '\'' +
+                ", idRequest=" + idRequest +
+                '}';
+    }
+}
