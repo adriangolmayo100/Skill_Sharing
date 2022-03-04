c4045c8bf77245ac2fe086d7aa0c7243614fef74
al394753 <al394753@uji.es>
2022-03-04 12:46:10 +0100

    Student

---
+++ src/main/java/es/uji/ei1027/SkillSharing/model/offer.java
@@ -0,0 +1,83 @@
+package es.uji.ei1027.SkillSharing.model;
+
+import java.sql.Date;
+
+public class offer {
+
+    private int idOffer;
+    private int idStudent;
+    private int idSkillType;
+    private String description;
+    private Date start;
+    private Date finish;
+    private int duration;
+
+    public int getIdOffer() {
+        return idOffer;
+    }
+
+    public void setIdOffer(int idOffer) {
+        this.idOffer = idOffer;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public int getIdSkillType() {
+        return idSkillType;
+    }
+
+    public void setIdSkillType(int idSkillType) {
+        this.idSkillType = idSkillType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getFinish() {
+        return finish;
+    }
+
+    public void setFinish(Date finish) {
+        this.finish = finish;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "offer{" +
+                "idOffer=" + idOffer +
+                ", idStudent=" + idStudent +
+                ", idSkillType=" + idSkillType +
+                ", description='" + description + '\'' +
+                ", start=" + start +
+                ", finish=" + finish +
+                ", duration=" + duration +
+                '}';
+    }
+}
