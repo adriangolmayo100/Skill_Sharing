a53a43d9c19d8e2cf7139e3ced5c708158bcace4
al394753 <al394753@uji.es>
2022-05-05 17:10:57 +0200

    Arreglos login

--- src/main/java/es/uji/ei1027/SkillSharing/controller/RegisterController.java
+++ src/main/java/es/uji/ei1027/SkillSharing/controller/RegisterController.java
@@ -1,9 +1,7 @@
 package es.uji.ei1027.SkillSharing.controller;
 
 import es.uji.ei1027.SkillSharing.dao.StudentDao;
-import es.uji.ei1027.SkillSharing.dao.UserDao;
 import es.uji.ei1027.SkillSharing.model.Student;
-import es.uji.ei1027.SkillSharing.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/es/uji/ei1027/SkillSharing/controller/UserController.java
+++ src/main/java/es/uji/ei1027/SkillSharing/controller/UserController.java
@@ -2,13 +2,10 @@ package es.uji.ei1027.SkillSharing.controller;
 
 import javax.servlet.http.HttpSession;
 
-import es.uji.ei1027.SkillSharing.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import es.uji.ei1027.SkillSharing.model.User;
 
 @Controller
 @RequestMapping("/tipos_usuario")
